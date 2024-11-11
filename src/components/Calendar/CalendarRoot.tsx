673dc1ec869898e725a39af548ec5034dcf63da1
Otavio Barbieri <otavi.barbieri.dev@gmail.com>
2024-11-11 16:55:15 -0300

    feat: add status for each sale

--- src/components/Calendar/CalendarRoot.tsx
+++ src/components/Calendar/CalendarRoot.tsx
@@ -1,44 +1,70 @@
-import { ReactNode, useState } from "react";
-import { } from "react-icons"
+import { ReactNode, useMemo, useState } from "react";
 import "./calendar.css";
-import { mockValues } from "../../data/mockValues";
+import { mockValues, type mockValuesType, PaymentStatusType } from "../../data/mockValues";
 import PaymentDetailBox from "../PaymentDetailBox/PaymentDetailBox";
 
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+
+enum WeekDays {
+
+  "domingo", "segunda-Feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado"
+}
 
 type WeekDay = {
   shortName: string;
   fullName: string;
   numberInTheWeek: number;
+  isVacationDay?: boolean
 };
 
+export type PaymentDays = {
+
+  date: string
+  value: number
+  ISODate: string
+  hasHoliday?: boolean
+  paidValues?: number[]
+  scheduledValues?: number[]
+}
+
 export default function CalendarRoot() {
+
+  console.log("Re-rendered")
+
   const [currentDate, setCurrentDate] = useState(new Date());
   const selectedDay = currentDate.getDate()
 
-  const convertDMYDateFormatToISO = (dateArray: { date: string, value: number }[]) => {
+  const transformIncomingDataIntoPaymentStucuture = (arr: mockValuesType[]): PaymentDays[] => {
 
-    return dateArray.map(item => {
+    return arr.map((item, _idx) => ({
 
-      const [ day, month, year ] = item.date.split("/")
-
-      return {
-        date: `${year}-${month}-${day}`,
-        value: item.value
+        date: item.date,
+        value: item.value,
+        ISODate: item.ISODate || convertDMYToISO(item.date),
+        hasHoliday: item.hasHoliday,
+        paidValues: item.status === "paid" ? [item.value] : [],
+        scheduledValues: item.status === "scheduled" ? [item.value] : []
       }
-    })
+    ))
   }
 
-  const salesArray = convertDMYDateFormatToISO(mockValues)
+  const convertDMYToISO = (date: string) => {
+
+    const [day, month, year] = date.split("/")
+
+    return `${year}-${month}-${day}`
+  }
+
+  const salesArray = useMemo(() => transformIncomingDataIntoPaymentStucuture(mockValues), [mockValues])
 
   const weekDays: WeekDay[] = [
-    { fullName: "Domingo", shortName: "Dom", numberInTheWeek: 0 },
-    { fullName: "Segunda-feira", shortName: "Seg", numberInTheWeek: 1 },
-    { fullName: "Terça-feira", shortName: "Ter", numberInTheWeek: 2 },
-    { fullName: "Quarta-feira", shortName: "Qua", numberInTheWeek: 3 },
-    { fullName: "Quinta-feira", shortName: "Qui", numberInTheWeek: 4 },
-    { fullName: "Sexta-feira", shortName: "Sex", numberInTheWeek: 5 },
-    { fullName: "Sábado", shortName: "Sab", numberInTheWeek: 6 },
+    { fullName: "Domingo", shortName: "Dom", numberInTheWeek: 0, isVacationDay: true },
+    { fullName: "Segunda-feira", shortName: "Seg", numberInTheWeek: 1, isVacationDay: false },
+    { fullName: "Terça-feira", shortName: "Ter", numberInTheWeek: 2, isVacationDay: false },
+    { fullName: "Quarta-feira", shortName: "Qua", numberInTheWeek: 3, isVacationDay: false },
+    { fullName: "Quinta-feira", shortName: "Qui", numberInTheWeek: 4, isVacationDay: false },
+    { fullName: "Sexta-feira", shortName: "Sex", numberInTheWeek: 5, isVacationDay: false },
+    { fullName: "Sábado", shortName: "Sab", numberInTheWeek: 6, isVacationDay: true },
   ];
 
   const months = [...Array(12).keys()].map((key) =>
@@ -66,42 +92,86 @@ export default function CalendarRoot() {
   const generateDaysArray = () => {
     const days = Array.from({ length: daysInMonth }, (_, index) => index + 1);
     const emptySpaces = Array.from({ length: firstDayOfMonth }).fill(null);
+
     return [...emptySpaces, ...days];
   };
 
-  const generateArrayOfYears = (startYear: number, rangeInYears: number = 20): number[] => {
-    return Array.from({ length: rangeInYears }, (_, idx) => startYear + idx);
-  };
+  const generateArrayOfYears = (startYear: number, rangeInYears: number = 20): number[] => Array.from({ length: rangeInYears }, (_, idx) => startYear + idx);
 
   const changeDay = (newDay: number) => {
 
-    const updatedDate = new Date(currentDate)
+    const updatedDate = new Date(currentDate);
 
-    updatedDate.setDate(newDay)
+    updatedDate.setDate(newDay);
 
-    setCurrentDate(updatedDate)
+    setCurrentDate(updatedDate);
   }
 
-  const checkIfHasSalesInTheDay = (day: number) => {
+  const generateDateToCheckWithISOFormat = (day: number) => new Date(currentYear, currentMonth, day).toISOString().split("T")[0];
+
+  /**
+   * 
+   * @param  day - A number representing the day wich is intended to search for sales
+   * @returns If the giving day has a sale or not
+   */
+  const checkIfHasSalesInTheDay = (day: number): { hasSale: boolean, status?: PaymentStatusType } | null => {
 
     try {
 
-      const dateToCheck = new Date(currentYear, currentMonth, day).toISOString().split("T")[0];
+      const dateToCheck = generateDateToCheckWithISOFormat(day)
+      const sale = salesArray.find((daysWithSale) => daysWithSale.ISODate === dateToCheck)
 
-      return salesArray.some((daysWithSale) => {
+      if (sale) {
 
-        const saleDate = new Date(daysWithSale.date).toISOString().split("T")[0];
+        const status = sale.paidValues?.length ? "paid" : sale.scheduledValues?.length ? "scheduled" : undefined
 
-        return saleDate === dateToCheck;
+        return { hasSale: true, status }
+      }
 
-      });
+      return { hasSale: false};
+
     } catch (err) {
 
       console.error(err);
 
-      return false;
+      return null;
     }
   };
+
+
+  /* const concatenateSalesWithSameStatus = (salesArr: PaymentDays[]) => {
+    return salesArr.reduce((acc, current, idx) => {
+
+      const dateToMatch = convertDMYToISO(current.date);
+      const existingEntry = [acc].find((item) => item.ISODate === dateToMatch)
+
+      if (existingEntry) {
+
+        existingEntry.value += current.value
+
+        if (mockValues[idx].status === "paid") {
+
+          existingEntry.paidValues = [...(existingEntry.paidValues || []), current.value]
+        } else if (mockValues[idx].status === "scheduled") {
+
+          existingEntry.scheduledValues = [...(existingEntry.scheduledValues || []), current.value]
+        }
+      } else {
+
+        [acc].push({
+
+          ...current,
+          ISODate: dateToMatch,
+          paidValues: mockValues[idx].status === "paid" ? [current.value] : [],
+          scheduledValues: mockValues[idx].status === "scheduled" ? [current.value] : []
+        })
+      }
+
+      return acc
+    })
+  }
+
+  console.log(concatenateSalesWithSameStatus(salesArray)) */
 
   return (
     <div className="flex flex-col md:flex-row gap-2 items-center">
@@ -161,35 +231,45 @@ export default function CalendarRoot() {
           </section>
 
           <section className="calendar__grid__monthdays gap-1 grid grid-cols-7 grid-rows-6 place-items-center">
-            {generateDaysArray().map((day, index) => (
-              <div
-                role="button"
-                tabIndex={day ? 0 : -1}
-                key={index}
-                {...(checkIfHasSalesInTheDay(Number(day)) ? { "data-sale": "true" } : {})}
-                {...day && selectedDay === Number(day) ? { "data-selected": "true" } : {}}
-                className={`calendar__day px-4 brightness-90 hover:opacity-75 w-[2ch] data-[selected="true"]:border-2 data-[selected="true"]:text-black data-[selected="true"]:bg-white rounded-md aspect-square flex items-center justify-center transition-opacity duration-500 data-[sale="true"]:after:bg-green-500 ${day === null ? "empty:pointer-events-none" : ""} `}
-                onClick={() => changeDay(day as number)}
-                title={day ? `${day} de ${months[currentMonth]} ${currentYear}` : undefined}
-              >
-                {day as unknown as ReactNode}
-              </div>
-            ))}
+            {generateDaysArray().map((day, index) => {
+
+              const dayInfo = day ? checkIfHasSalesInTheDay(Number(day)) : null
+
+              return (
+
+                <div
+                  role="button"
+                  tabIndex={day ? 0 : -1}
+                  key={index}
+                  {...(dayInfo?.hasSale ? { "data-sale": "true" } : {})}
+                  {...day && selectedDay === Number(day) ? { "data-selected": "true" } : {}}
+                  {...dayInfo?.status === "paid" ? { "data-status": "paid"} : { "data-status": "scheduled"}}
+                  className={`calendar__day px-4 brightness-90 hover:opacity-75 w-[2ch] data-[selected="true"]:border-2 data-[selected="true"]:text-black data-[selected="true"]:bg-white rounded-md aspect-square flex items-center justify-center transition-opacity duration-500 data-[sale="true"]:after:bg-green-500 data-[sale="scheduled"]:after:bg-gray-300 ${day === null ? "empty:pointer-events-none" : ""} `}
+                  onClick={() => changeDay(day as number)}
+                  title={day ? `${day} de ${months[currentMonth]} ${currentYear}` : undefined}
+                >
+                  {day as unknown as ReactNode}
+                </div>
+                )
+            }
+            )}
           </section>
         </div>
 
         <footer className="calendar__footer">
           <ul className="calendar__footer__list flex items-center gap-2">
-            
-            <li><div className="calendar__legend flex items-baseline gap-1 leading-none"><span className="calendar__legend__icon rounded-full bg-green-500 block w-2 h-2"></span><small className="brightness-50">devendo</small></div></li>
+
+            <li><div className="calendar__legend flex items-baseline gap-1 leading-none"><span className="calendar__legend__icon rounded-full bg-green-500 block w-2 h-2"></span><small className="brightness-50">pago</small></div></li>
+
+            <li><div className="calendar__legend flex items-baseline gap-1 leading-none"><span className="calendar__legend__icon rounded-full bg-gray-500 border block w-2 h-2"></span><small className="brightness-50">a receber</small></div></li>
           </ul>
         </footer>
       </article>
 
       <PaymentDetailBox
-        keyAndValues={[{label: "Total", value: "69.90"}, {label: "Pago", "value": "69.99"} ]}
+        keyAndValues={[{ label: "Total", value: "69.90" }, { label: "Pago", "value": "69.99" }]}
         date={currentDate.toLocaleDateString("pt-br")}
-        
+
       />
     </div>
   );
--- src/data/mockValues.ts
+++ src/data/mockValues.ts
@@ -1,13 +1,27 @@
-export const mockValues = [
-    { date: "01/11/2024", value: 1233.00 },
-    { date: "03/11/2024", value: 1233.00 },
-    { date: "07/11/2024", value: 1233.00 },
-    { date: "10/11/2024", value: 1233.00 },
-    { date: "12/11/2024", value: 1233.00 },
-    { date: "15/11/2024", value: 1233.00 },
-    { date: "18/11/2024", value: 1233.00 },
-    { date: "21/11/2024", value: 1233.00 },
-    { date: "25/11/2024", value: 1233.00 },
-    { date: "30/11/2024", value: 1233.00 },
-    { date: "29/12/2024", value: 69.00}
+export type PaymentStatusType = "paid" | "scheduled"
+
+export type mockValuesType = {
+
+    date: string
+    value: number
+    status: PaymentStatusType
+    hasHoliday?: boolean
+    ISODate?: string
+}
+
+export const mockValues: mockValuesType[] = [
+    { date: "21/12/2022", value: 204.00, status: "paid" },
+    { date: "01/11/2024", value: 1233.00, status: "paid" },
+    { date: "03/11/2024", value: 1233.00, status: "scheduled" },
+    { date: "07/11/2024", value: 1233.00, status: "paid" },
+    { date: "10/11/2024", value: 1233.00, status: "paid" },
+    { date: "12/11/2024", value: 1233.00, status: "scheduled" },
+    { date: "13/11/2024", value: 1233.00, status: "scheduled" },
+    { date: "15/11/2024", value: 1233.00, status: "paid", hasHoliday: true },
+    { date: "18/11/2024", value: 1233.00, status: "scheduled" },
+    { date: "21/11/2024", value: 1233.00, status: "paid" },
+    { date: "25/11/2024", value: 1233.00, status: "scheduled" },
+    { date: "30/11/2024", value: 1233.00, status: "paid" },
+    { date: "29/12/2024", value: 69.00, status: "scheduled"},
+    { date: "22/01/2025", value: 65.77, status: "scheduled"}
 ]
